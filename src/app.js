dbacffad9acb1db7410b9fb51f920c5355e3498e
Sümeyye DUMANLI <engineerbees@gmail.com>
2021-10-19 00:06:47 +0300

    18 Ekim 2021 ders sonu

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Hellöö")
+
+let dolarDun=9.20
+let dolarBugun=9.30
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun =11.2
+//euroDun=11 >>değişmiyormuş
+console.log(euroDun)
+
+//array
+let konutKredileri= ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi"]
+console.log(konutKredileri)
+
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("<li>"+konutKredileri[index]+"</li>")
+}
+console.log("</ul>")
+
+{
+    /*<ul>
+        <li></li>
+        <li></li>
+    */
+}
